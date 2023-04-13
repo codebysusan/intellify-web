070e3139108187ea656de30f7d168a2236f10d6a
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-04-14 01:10:35 +0530

    rewritten code to use fetch instead of axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import { Link } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Link } from "react-router-dom";
 
 function App() {
   const heroSection = (
@@ -11,11 +11,13 @@ function App() {
         </p>
       </div>
       <div className='md:mt-16 text-dark mt-10 px-10 md:px-96 text-xl '>
-        Our app is designed to help you keep track of your health information. Through the modern use of AI, we are able to provide you with the most accurate and up to date information.
+        Our app is designed to help you keep track of your health information.
+        Through the modern use of AI, we are able to provide you with the most
+        accurate and up to date information.
       </div>
       <div className='flex justify-center pt-12'>
-        <Link to="/signup">
-          <button className='bg-slate-50 hover:bg-blue-800 hover:text-white font-medium md:text-2xl md:py-4 md:px-8 md:font-medium text-md py-3 px-6 rounded-md' >
+        <Link to='/signup'>
+          <button className='bg-slate-50 hover:bg-blue-800 hover:text-white font-medium md:text-2xl md:py-4 md:px-8 md:font-medium text-md py-3 px-6 rounded-md'>
             Get Started
           </button>
         </Link>
@@ -24,13 +26,11 @@ function App() {
   );
 
   return (
-    <div className="App h-screen">
+    <div className='App h-screen'>
       <Navbar />
-      {
-        heroSection
-      }
+      {heroSection}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
